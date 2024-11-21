654274d79011b25cd1c901cc9f5960cb722af03d
hyeri <jeonghyeri98@gmail.com>
2024-11-22 06:54:34 +0900

    init

---
+++ src/main/kotlin/com/hyeri/learningcoroutine/LearningCoroutineApplication.kt
@@ -0,0 +1,11 @@
+package com.hyeri.learningcoroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningCoroutineApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearningCoroutineApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyeri/learningcoroutine/LearningCoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyeri.learningcoroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningCoroutineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
